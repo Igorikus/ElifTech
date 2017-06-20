2bfaf4c7169ecddc5f07382c0cf36aec68004625
Igorikus <slayeryuskevych@gmail.com>
2017-06-21 00:30:25 +0300

    input form

---
+++ ElifTech/public_html/test.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+    
+    
+    
+    
+    
+});
+
+ var myLink = $('<a/>').text('delivary').addClass('navigation').attr('href', 'google.com');
+            
+            var listEl = $('<li/>');
+            listEl.append(myLink);
+            $('.container').append(listEl);
+        });
